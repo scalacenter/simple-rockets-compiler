7b9ab7c0b0e232a566dc91b755198ccd6dee330e
Anatolii Kmetiuk <anatoliykmetyuk@gmail.com>
2022-02-03 21:37:49 +0100

    Essential craft properties added

--- core/src/main/scala/rocketscompiler/dsl/data.scala
+++ core/src/main/scala/rocketscompiler/dsl/data.scala
@@ -14,9 +14,22 @@ val Heading = Autopilot("heading")
 // Heading
 val Retrograde = "Retrograde"
 
+// Craft & Environment Information
 object Fuel:
   val InStage = CraftProperty("Fuel.FuelInStage", "prop-fuel")
 
 object Altitude:
   val ASL = CraftProperty("Altitude.ASL", "prop-altitude")
   val AGL = CraftProperty("Altitude.AGL", "prop-altitude")
+
+object Orbit:
+  val Apoapsis = CraftProperty("Orbit.Apoapsis", "prop-orbit")
+  val Periapsis = CraftProperty("Orbit.Periapsis", "prop-orbit")
+  val TimeToApoapsis = CraftProperty("Orbit.TimeToApoapsis", "prop-orbit")
+
+object Performance:
+  val StageDeltaV = CraftProperty("Performance.StageDeltaV", "prop-performance")
+  val BurnTime = CraftProperty("Performance.BurnTime", "prop-performance")
+
+object Velocity:
+  val OrbitVelocity = CraftProperty("Vel.OrbitVelocity", "prop-velocity")
